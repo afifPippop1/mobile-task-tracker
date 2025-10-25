e468f25d3f1fb7539c6ae400e44a2a226d65a85b
afifPipop1 <afifpippop@gmail.com>
2025-10-25 12:24:01 +0700

    style: 💄 init gluestack

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from "@/components/ui/text";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Home() {
+  return (
+    <SafeAreaView>
+      <Text>Hello</Text>
+    </SafeAreaView>
+  );
+}
